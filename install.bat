793bef9c9869feaabe07d49fb296b2b3b5d19997
a7med <34asqf@gmail.com>
2026-06-26 03:49:53 +0300

    chore: remove node-portable binaries and temp files from git tracking

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "PATH=%~dp0node-portable\node-v20.19.2-win-x64;%PATH%"
-echo Installing dependencies...
-npm install
-echo.
-echo Installation complete! You can now close this window and run start.bat
-pause
